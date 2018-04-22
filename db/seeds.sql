6bfe3dbc292fc246b940cabc2f01d9e680ad46ea
aliperez <alicia.perez@utexas.edu>
2018-04-22 15:18:30 -0700

    all working

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Cheddar Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Arugula Bluecheese Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Southwest Burger', false);
+
+
