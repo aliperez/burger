165f5d82530a56440d72333bbfd63b88f9121b50
aliperez <alicia.perez@utexas.edu>
2018-04-23 16:13:07 -0700

    added heroku code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -28,7 +28,7 @@ router.post("/api/burgers", function(req, res) {
   ], [
     req.body.name
   ], function(result) {
-    // Send back the ID of the new quote
+    // Send back the ID of the new burger
     res.json({ id: result.insertId });
   });
 });
